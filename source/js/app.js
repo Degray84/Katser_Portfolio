dac427fbf58e06a3e71d53b68bae3644478456c1
Katser84 <gray84@tut.by>
2017-07-03 06:33:59 +0300

    Сделал flip эффект на странице index

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,16 @@
-// (function() {
-//   'use strict';
-
-//   setTimeout(function() {
-//     document.querySelector('.greating_picture').classList.add('m--show');
-//   }, 1000);
-// })();
+(function() {
+    'use strict';
+    var indexCard = document.getElementsByClassName("card-flip"),
+        authoriz = document.getElementsByClassName("login"),
+        indexBack = document.getElementsByClassName("card-login-nav__link")
+    $(authoriz).click(function(e) {
+        e.preventDefault();
+        $(indexCard).css("transform", "rotateY(180deg)");
+        $(this).fadeOut(1000);
+    });
+    $(indexBack).click(function(e) {
+        e.preventDefault();
+        $(indexCard).css("transform", "rotateY(0deg)");
+        $(authoriz).fadeIn(1000);
+    });
+})();
