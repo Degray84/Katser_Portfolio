f3a4d22d1d1a2f02a6ca466504f7727ec6e85c6f
Katser84 <gray84@tut.by>
2017-07-29 09:36:11 +0300

    Добавил анимацию списков в _animation.js, сделал анимацию фона меню гамбургера и анимацию списка меню

--- source/js/modules/_animation.js
+++ source/js/modules/_animation.js
@@ -1,9 +1,28 @@
     function animate(content, type) {
         content.classList.add('animated');
         content.classList.add(type);
-        setTimeout(function() {
+        setTimeout(function () {
             content.classList.remove(type);
             content.classList.remove('animated');
         }, 1000)
     }
-    export { animate };
+
+    function animateList(list, type, speed) {
+        let i = 0;
+        (function aniList() {
+            const iMax = list.length - 1;
+            list[i].style.display = 'block';
+            animate(list[i], type)
+            setTimeout(function () {
+                if (i < iMax) {
+                    i++;
+                    aniList();
+                }
+            }, speed);
+        }())
+    }
+
+    export {
+        animate,
+        animateList
+    };
--- source/js/modules/_hamburger.js
+++ source/js/modules/_hamburger.js
@@ -1,13 +1,27 @@
-export default function() {
+import {
+    animate,
+    animateList
+} from './_animation';
+export default function () {
     let icon = document.querySelector('.hamburger-icon'),
         menu = document.querySelector('.hero__hideMenu'),
-        menuItem = document.querySelectorAll('.hero__hideMenu');
+        menuItems = document.querySelectorAll('.hideMenu-nav__item'),
+        bgLeft = menu.querySelector('.hero__menuBg_left'),
+        bgRight = menu.querySelector('.hero__menuBg_right');
     //  При клике на иконку  делает активным меню и иконку, при втором клике убирает активный класс
     function _setUpListeners() {
-        icon.addEventListener('click', function(ev) {
+        icon.addEventListener('click', function (ev) {
             ev.preventDefault();
             menu.classList.toggle('active');
             icon.classList.toggle('hamburger-icon_active');
+            animate(bgLeft, 'slideInLeft');
+            animate(bgRight, 'slideInRight');
+            menuItems.forEach(function (element) {
+                element.style.display = 'none';
+            });
+            setTimeout(() => {
+                animateList(menuItems, 'rubberBand', 200);
+            }, 1000);
         })
     }
 
